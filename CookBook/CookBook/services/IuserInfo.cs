7ea4e3d141a3d21ea21654897a9069c6a8a112c4
utshav khatiwada <utshavkhatiwada4@gmail.com>
2024-04-04 10:01:01 -0400

    added more code

--- CookBook/CookBook/services/IuserInfo.cs
+++ CookBook/CookBook/services/IuserInfo.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using CookBook.Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CookBook.services
 {
-    class IuserInfo 
+    public interface IUserInfo
     {
-
-
+        Task<bool> UserExists(string email, string password);
+        Task AddUser(UserDB user);
     }
 }
