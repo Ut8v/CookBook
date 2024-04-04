79802f7ae95461276995589563f74315b2405b63
utshav khatiwada <utshavkhatiwada4@gmail.com>
2024-04-04 09:55:11 -0400

    Interface

--- CookBook/CookBook/Model/UserDBContext.cs
+++ CookBook/CookBook/Model/UserDBContext.cs
@@ -1,11 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using CookBook.services;
 using SQLite;
+using System.IO;
 
 namespace CookBook.Model
 {
-    internal class UserDBContext
+    internal class UserDBContext : IUserInfo
     {
+        SQLiteAsyncConnection database;
+
+        public UserDBContext()  
+        {
+            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "UserDB.db");
+            database = new SQLiteAsyncConnection(dbPath);
+            database.CreateTableAsync<UserDB>().Wait();
+        }
+        //to find if user exists
+        public async Task<bool> UserExists(string email, string password)
+        {
+            var user = await database.Table<UserDB>().FirstOrDefaultAsync(u => u.Email == email && u.Password == password);
+            return user != null;
+        }
+        //add user to the database
+        public async Task AddUser(UserDB user)
+        {
+            await database.InsertAsync(user);
+        }
     }
 }
