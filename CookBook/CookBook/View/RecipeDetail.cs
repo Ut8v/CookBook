f28cba7e2b454bf79a93ed10fbd75e746fbcecf6
patelsheel <patel7sa@mail.uc.edu>
2024-04-12 15:02:53 -0400

    Updated RecipePage to dynamically render each recipe, added RecipeDetail class and RecipeManager class to handle the dynamic rendering of recipies.  Also, added comments.
    Utsav, please check MainPage.xaml.cs line 35-37.

---
+++ CookBook/CookBook/View/RecipeDetail.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CookBook.Model
+{
+    public class RecipeDetail
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<string> Ingredients { get; set; }
+        public List<string> Instructions { get; set; }
+    }
+}
